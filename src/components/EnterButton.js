b60e20447093b58ddc86ffb01adf6338c9862763
Ben DiFrancesco <ben@scopelift.co>
2019-05-10 15:03:55 -0400

    Get rid of nested reducers-- use a single reducer for the calculator memory

--- src/components/EnterButton.js
+++ src/components/EnterButton.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { pressEnter } from '../actions/entryBufferActions';
-import entryBufferReducer from '../reducers/entryBufferReducer';
 
 class EnterButton extends Component {
     
--- src/reducers/calcMemReducer.js
+++ src/reducers/calcMemReducer.js
@@ -1,21 +1,70 @@
 import { PRESS_NUMBER, PRESS_ENTER, PRESS_OPERATOR } from '../actions/types';
-import {default as entryBufferReducer, initialState as entryBufferInitialState } from './entryBufferReducer';
-import {default as stackReducer, initialState as stackInitialState} from './stackReducer';
 
 const initialState = {
-    entryBuffer: entryBufferInitialState,
-    stack: stackInitialState,
+    entryBuffer: "",
+    stack: [],
 }
 
 export default function(state = initialState, action) {
     switch (action.type) {
         case PRESS_NUMBER:
+            return {
+                entryBuffer: entryBufferPressNumber(state.entryBuffer, action.payload),
+                stack: state.stack,
+            }
         case PRESS_ENTER:
+            return {
+                entryBuffer: initialState.entryBuffer,
+                stack: stackPressEnter(state.stack, state.entryBuffer)
+            }      
         case PRESS_OPERATOR:
+            return {
+                entryBuffer: entryBufferPressOperator(state.entryBuffer, state.stack.length),
+                stack: stackPressOperator(state.stack, state.entryBuffer, action.payload),
+            }
         default:
-            return { 
-                        entryBuffer: entryBufferReducer(state.entryBuffer, action), 
-                        stack: stackReducer(state.stack, action),
-                    }
+            return state;
     }
+}
+
+function entryBufferPressNumber(currentBuffer, nextNumber) {
+    return parseInt(currentBuffer + nextNumber.toString()).toString();
+}
+
+function entryBufferPressOperator(currentBuffer, stackLength) {
+    if (stackLength === 0) {
+        return currentBuffer;
+    } else {
+        return initialState.entryBuffer;
+    }
+}
+
+function stackPressEnter(currentStack, entryBuffer) {
+    let newNumber = parseInt(entryBuffer);
+
+    if (isNaN(newNumber)) {
+        return currentStack;
+    }
+
+    return [...currentStack, newNumber];
+}
+
+function stackPressOperator(currentStack, entryBuffer, operation) {
+    let newStack = [...currentStack];
+    let newNumber = parseInt(entryBuffer);
+
+    if (!isNaN(newNumber)) {
+        newStack.push(newNumber);
+    }
+
+    if (newStack.length < 2) {
+        return currentStack;
+    }
+
+    let right = newStack.pop();
+    let left = newStack.pop();
+    let newValue = operation(left, right);
+
+    newStack.push(newValue);
+    return newStack;
 }
