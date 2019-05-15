ad4db88b1bd8db8fb5b3d7bfbeb64427682ca1e8
Ben DiFrancesco <ben@scopelift.co>
2019-05-15 13:09:39 -0400

    Prevent RAND button from being clicked again during fetch

--- src/components/RandButton.js
+++ src/components/RandButton.js
@@ -10,19 +10,23 @@ class RandButton extends Component {
 
     onClick(event) {
         event.preventDefault();
+
+        if (this.props.isFetching) {
+            return;
+        }
+
         this.props.getRand();
     }
 
     render() {
         let loadingIndicator = (<span className="glyphicon glyphicon-refresh" aria-hidden="true"></span>);
-
+        let content = this.props.isFetching ? loadingIndicator : "RAND";
         return (
             <div>
                 <button type="button" 
                         className="btn btn-info col col-md-1 col-sm-1 col-xs-2" 
                         onClick={this.onClick}>
-                    {this.props.isFetching ? loadingIndicator : ""}{" "}
-                    RAND
+                    {content}
                 </button>
             </div>
         );
